f45764f4ba255e8961517edafc710ff54840fbb5
GhostCrab <Ryan.Pielow@gmail.com>
2022-02-25 16:45:42 -0800

    update

--- src/buy_server.ts
+++ src/buy_server.ts
@@ -5,7 +5,10 @@ export async function main(ns: NS): Promise<void> {
 
     const cash = ns.getServerMoneyAvailable("home");
 
-    if (isNaN(count)) count = 1;
+    if (isNaN(count)) {
+		ns.tprintf("count was not a number")
+		count = 1;
+	}
 
     if (isNaN(ram) && ns.args[0] !== "max") {
         // ns.tprintf("Calculating maximum ram allocation for new server (Cash: %s)", ns.nFormat(cash, '($0.000a)'))
@@ -52,7 +55,9 @@ export async function main(ns: NS): Promise<void> {
         ns.tprintf("run buy_server.js %d %d %s", ram, maxServers, ns.nFormat(cost * maxServers, "($0.000a)"));
 
         return;
-    } else {
+    }
+
+    if (ns.args[0] === "max") {
         ram = 0;
         for (let i = 0; i <= 20; i++) {
             const curRam = Math.pow(2, i);
@@ -60,36 +65,14 @@ export async function main(ns: NS): Promise<void> {
             if (cost <= cash) ram = curRam;
             else break;
         }
-        const cost = ns.getPurchasedServerCost(ram);
-        const pservers = ns.getPurchasedServers().sort((a, b) => ns.getServerMaxRam(a) - ns.getServerMaxRam(b));
-        const nextIdx = pservers.length;
-        let nextServerName = "pserv-" + nextIdx;
-        if (pservers.length === ns.getPurchasedServerLimit()) {
-            const delServer = ns.getServer(pservers[0]);
-            if (delServer.maxRam >= ram) {
-                ns.tprintf(
-                    "Max servers reached and new server is not an improvement (%dGB/%dGB ram)",
-                    delServer.maxRam,
-                    ram
-                );
-                return;
-            }
-            ns.tprintf("Deleting server %s with %dGB ram", delServer.hostname, delServer.maxRam);
-            nextServerName = delServer.hostname;
-            ns.killall(delServer.hostname);
-            ns.deleteServer(delServer.hostname);
-        }
-        const hostname = ns.purchaseServer(nextServerName, ram);
-        ns.tprintf("Purchased server %s with %d ram for %s", hostname, ram, ns.nFormat(cost / count, "($0.000a)"));
-
-        return;
     }
 
     const cost = ns.getPurchasedServerCost(ram) * count;
     if (cash < cost) {
         ns.tprintf(
-            "Unable to purchase %d server(s) with %d ram (%s < %s)",
+            "Unable to purchase %d server%s with %d ram (%s < %s)",
             count,
+			count > 1 ? "s" : '',
             ram,
             ns.nFormat(cash, "($0.000a)"),
             ns.nFormat(cost, "($0.000a)")
@@ -101,10 +84,8 @@ export async function main(ns: NS): Promise<void> {
         const pservers = ns.getPurchasedServers().sort((a, b) => ns.getServerMaxRam(a) - ns.getServerMaxRam(b));
         const nextIdx = pservers.length;
         let nextServerName = "pserv-" + nextIdx;
-
         if (pservers.length === ns.getPurchasedServerLimit()) {
             const delServer = ns.getServer(pservers[0]);
-
             if (delServer.maxRam >= ram) {
                 ns.tprintf(
                     "Max servers reached and new server is not an improvement (%dGB/%dGB ram)",
@@ -113,14 +94,14 @@ export async function main(ns: NS): Promise<void> {
                 );
                 return;
             }
-
             ns.tprintf("Deleting server %s with %dGB ram", delServer.hostname, delServer.maxRam);
             nextServerName = delServer.hostname;
             ns.killall(delServer.hostname);
             ns.deleteServer(delServer.hostname);
         }
-
         const hostname = ns.purchaseServer(nextServerName, ram);
         ns.tprintf("Purchased server %s with %d ram for %s", hostname, ram, ns.nFormat(cost / count, "($0.000a)"));
+
+        return;
     }
 }
