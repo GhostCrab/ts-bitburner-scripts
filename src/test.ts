d9339b1c543e8066342de19b6e00d865b2754f67
Ryan Pielow <Ryan.Pielow@gmail.com>
2022-03-08 08:10:55 -0800

    update

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,30 @@
 import { NS } from "@ns";
 
+function findProp(propName: string) {
+    for (const div of eval("document").querySelectorAll("div")) {
+        const propKey = Object.keys(div)[1];
+        if (!propKey) continue;
+        const props = div[propKey];
+        if (props.children?.props && props.children.props[propName]) return props.children.props[propName];
+        if (props.children instanceof Array)
+            for (const child of props.children) if (child?.props && child.props[propName]) return child.props[propName];
+    }
+}
+
 export async function main(ns: NS): Promise<void> {
     ns.tprintf("TEST: %s", ns.heart.break());
 
-    await ns.grow("ecorp");
-    // ns.tprintf("getWeakenTime %s", ns.getWeakenTime("ecorp"));
-    // ns.tprintf("getServerSuppression %s", ns.getServerSuppression("ecorp"));
-    // ns.tprintf("suppressAnalyze %s", ns.suppressAnalyze("ecorp", 100, 100));
+    const playerProp = findProp("player");
+    const agDivName = "Tobacco";
+
+    if (playerProp?.corporation?.divisions) {
+        const agDiv: Industry = playerProp.corporation.divisions.find((a: { type: string }) => a.type === agDivName);
+
+        if (agDiv) {
+            for (const [city, product] of Object.entries(agDiv.products)) {
+                if (product)
+                    product.marketTa2 = false;
+            }
+        }
+    }
 }
